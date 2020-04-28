50cb89b50d9215db8f8ae8a6f2603efc95e668a4
Josh Blumberg <jlblumberg@gmail.com>
2020-04-28 17:32:54 +0100

    Add test and functionality of operators to keypad component

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -17,12 +17,18 @@ describe('Keypad', () => {
     );
   });
 
-  it('should render two <div />\'s', () => {
-    expect(wrapper.find('div').length).toEqual(2);
+  it('should render 3 <div />\'s', () => {
+    expect(wrapper.find('div').length).toEqual(3);
   });
 
   it('renders the values of the numbers', () => {
     wrapper.setProps({numbers: [0, 1, 2]});
     expect(wrapper.find('.numbers-container').text()).toEqual('012');
   })
+
+  it('renders the values of operators', () => {
+    wrapper.setProps({ operators: ['+', '-', '*', '/'] });
+    expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
+  });
+
 });
